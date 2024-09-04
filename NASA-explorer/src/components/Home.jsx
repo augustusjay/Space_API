120c7f1be34ed38af201291c2660586029d4e26e
Augustus Jay Del Rosario <delr0016@algonquinlive.com>
2024-09-04 10:03:06 -0400

    Created a Nasa Explorer project

---
+++ NASA-explorer/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home(props) {
+	const { data } = props;
+	return (
+		<div className="imgContainer">
+			<img src={data.hdurl} alt={data.title || "bg-img"} className="bgImage" />
+		</div>
+	);
+}
